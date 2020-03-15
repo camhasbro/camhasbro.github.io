ea98e5f9ec94ddb2934b24a745c425d861515422
Conner Hasbrouck <connerhasbrouck@gmail.com>
2020-03-15 15:16:51 -0500

    portfolio

---
+++ js/fullPageAnchors.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+  $('#fullpage').fullpage({
+    sectionsColor: ['#ffffff', '#000000'],
+    anchors: ['about', 'work', 'contact'],
+    menu: '#menu',
+    scrollOverflow: true,
+    resetSliders: true,
+  });
+});
